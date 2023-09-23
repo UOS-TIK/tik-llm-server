6b5f576c316c3a4f7b7a6543a0a21bdc6a837713
2langk <leelk980@gmail.com>
2023-09-23 13:13:36 +0900

    [feat] prompt 정리

--- src/primary/finish-interview/finish-interview.port.ts
+++ src/primary/finish-interview/finish-interview.port.ts
@@ -1,4 +1,4 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { MemoryStoreManager } from '@src/secondary';
 import { FinishInterviewData, FinishInterviewView } from './finish-interview.data';
 
@@ -11,18 +11,15 @@ export class FinishInterviewPort {
       type: 'interviewPaper',
       id: data.interviewId,
     });
-    if (interviewPaper.filter((each) => each.isCompleted === false).length) {
-      throw new BadRequestException(`interview is not finished. id=${data.interviewId}`);
-    }
+    // if (interviewPaper.filter((each) => each.isCompleted === false).length) {
+    //   throw new BadRequestException(`interview is not finished. id=${data.interviewId}`);
+    // }
 
-    const interviewHistory = await this.memoryStoreManager.get({
-      type: 'interviewHistory',
-      id: data.interviewId,
-    });
+    // const interviewHistory = await this.memoryStoreManager.get({
+    //   type: 'interviewHistory',
+    //   id: data.interviewId,
+    // });
 
-    console.log(4444, JSON.stringify(interviewPaper));
-    console.log(5555, interviewHistory);
-
-    return { result: interviewHistory.join(', ') };
+    return { result: JSON.stringify(interviewPaper) };
   }
 }
--- src/primary/speak-to-interviewer/speak-to-interviewer.port.ts
+++ src/primary/speak-to-interviewer/speak-to-interviewer.port.ts
@@ -19,9 +19,9 @@ export class SpeakToInterviewerPort {
         throw new BadRequestException(`interview is not initialized. id=${data.interviewId}`);
       });
 
-    const itemIndex = interviewPaper.findIndex((each) => each.isCompleted === false);
-    const currInterviewItem = interviewPaper[itemIndex];
-    const nextInterviewItem = interviewPaper[itemIndex + 1] ?? null;
+    const currItemIndex = interviewPaper.findIndex((each) => each.isCompleted === false);
+    const currInterviewItem = interviewPaper[currItemIndex];
+    const nextInterviewItem = interviewPaper[currItemIndex + 1] ?? null;
     if (!currInterviewItem) {
       throw new BadRequestException(`interview is finished. id=${data.interviewId}`);
     }
@@ -43,7 +43,7 @@ export class SpeakToInterviewerPort {
       }),
     );
 
-    interviewPaper[itemIndex] = result.currInterviewItem;
+    interviewPaper[currItemIndex] = result.currInterviewItem;
 
     await this.memoryStoreManager.set({
       type: 'interviewPaper',
@@ -71,55 +71,53 @@ export class SpeakToInterviewerPort {
 당신은 기술 면접을 진행중인 시니어 개발자입니다.
 아래 주어진 정보를 이용하여 자연스럽게 면접을 진행해주세요.
 
-###면접 질문항목(InterviewItem):
-1. currInterviewItem
-- 현재 대화 중인 면접 질문 및 평가 정보입니다.
-- 대화를 통해 currInterviewItem을 채우는 것이 목표입니다.
-- 적절한 추가 질문(tailQuestion)을 통해 깊이있는 면접을 진행하세요.
-- 적절한 tailQuestion이 없거나 이 주제로 충분히 대화를 나누었다면 isCompleted를 true로 변경해주세요.
-**currInterviewItem: ${JSON.stringify(params.currInterviewItem)}
-
-2. nextInterviewItem
-- 현재 주제에 대한 대화를 마무리할때, 이어나갈 질문 정보입니다.
-- nextInterviewItem은 currInterviewItem가 completed 됐을때 다음 질문을 위한 주제로 이용해주세요.(이때 만약 nextInterviewItem가 null이라면 면접을 종료해주세요)
-**nextInterviewItem: ${JSON.stringify(params.nextInterviewItem)}
-
-3. InterviewItem의 property에 대한 설명입니다.
+###면접 질문 주제(InterviewItem):
 type InterviewItem = {
   question: string; // 면접관이 해야할 질문입니다.
   answer: string; // 지원자의 답변입니다.
-  comment: string; // 지원자의 답변에 대한 면접관의 평가 및 피드백입니다.(최대한 자세히 작성해주세요)
-  score: number; // 지원자의 답변에 대한 점수입니다. (10점 만점이며, 0점이면 아직 평가전입니다)
-  tailQuestions: { // 지원자의 답변을 듣고 추가로 하고 싶은 질문이 있다면 추가해주세요. (먼저한 질문이 앞에 있도록 순서를 유지해주세요)
+  comment: string; // 지원자의 답변에 대한 면접관의 평가 및 피드백입니다.
+  score: number; // 지원자의 답변에 대한 점수입니다.(10점 만점이며, 0점이면 아직 평가전입니다)
+  tailQuestions: { // 지원자의 답변을 듣고 추가로 묻고 싶은 질문이 있다면 추가해주세요.
     question: string;
     answer: string;
     comment: string;
     score: number;
   }[];
-  isCompleted: boolean // 기본값은 false이며, 더 이상 해당 질문의 꼬리 질문(tailQuestions)이 없고 현재 주제에 대한 대화를 끝내고 싶다면 true로 변경해주세요.
+  isCompleted: boolean // 해당 주제가 완료되었는지 여부입니다. 더 이상 필요한 꼬리 질문(tailQuestions)이 없고, 현재 주제에 대한 대화를 끝내고 다음 주제로 넘어가고 싶다면 true로 변경해주세요.
 };
 
-###이전 대화 기록(최근 4개):
-${JSON.stringify(params.interviewHistory.slice(-5, -1))}
+###현재 면접 질문 주제(CurrInterviewItem):
+- 현재 대화 중인 주제, 질문 및 평가 정보입니다.
+- 지원자와의 대화를 통해 currInterviewItem의 비어있는 항목을 채우는 것이 첫번쨰 당신의 목표입니다.
+
+**currInterviewItem: 
+${JSON.stringify(params.currInterviewItem)}
+
+###다음 면접 질문 주제(NextInterviewItem):
+- 현재 대화 중인 주제를 마무리 했을때 이어서 질문할 주제입니다.
+- currInterviewItem의 isCompleted가 true가 됐을때 nextInterviewItem의 question을 이용해 질문을 이어나가세요.(단, nextInterviewItem이 null이라면 면접을 마무리하고 면접 종료를 안내하세요!)
+
+**nextInterviewItem:
+${JSON.stringify(params.nextInterviewItem)}
 
 ###현재 대화 내용:
-**면접관이 한말
-${params.interviewHistory.slice(-1)}
+- 전체 맥락을 고려해 지원자가 마지막으로 한 말에 적절한 답변을 하는것이 당신의 두번째 목표입니다.
 
-**지원자가 한말:
-${params.message}
-
+**messages: 
+${[...params.interviewHistory.slice(-5), `지원자가 마지막으로 한말: ${params.message}`].join('\n')}
 
 ###응답 예시:
 응답은 아래 JSON 양식을 따라 주세요.
 1. currInterviewItem: (필수)
-- 적절하게 currInterviewItem의 항목을 업데이트 해주세요.
-- isCompleted가 true이고 nextInterviewItem이 null이라면 면접을 마무리하고 지원자에게 면접 종료를 안내해주세요.
+- 지원자가 마지막으로 한말을 보고 수정할 currInterviewItem 입니다. 적절하게 currInterviewItem의 항목을 업데이트 해주세요.
+- 추가 질문(tailQuestion)을 통해 깊이 있는 면접을 진행해주세요. 2~3개의 꼬리질문을 해주세요.
+- 만약, isCompleted가 true이고 다음 주제(nextInterviewItem)가 null이라면 면접을 마무리하고 지원자에게 면접 종료를 안내해주세요.
 
 2. reply: (필수)
-- 현재 대화 내용의 지원자가 한말에 적절한 대답을 해주세요.
-- 면접의 진행을 위해 현재 주제(currInterviewItem)에 대한 추가 질문(tailQuestions)을 하거나 현재 주제를 마무리하고 다음 주제에 대해 질문을 해주세요.
-- 만약, isCompleted가 true이고 다음 주제가 null이라면 면접을 마무리하고 지원자에게 면접 종료를 안내해주세요.
+- 현재 대화 내용의 지원자가 마지막으로 한말에 해줄 대답입니다.
+- 자연스러운 면접의 진행을 위해 현재 주제(currInterviewItem)에 대한 추가 질문(tailQuestions)을 하거나 현재 주제를 마무리하고 다음 주제에 대한 질문을 해주세요.
+- 질문은 명확하게 하나씩만 해주세요.
+- 만약, isCompleted가 true이고 다음 주제(nextInterviewItem)가 null이라면 면접을 마무리하고 지원자에게 면접 종료를 안내해주세요.
 
 {
   "currInterviewItem": {}, 

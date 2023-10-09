2de24d913543e852755c33343d9773a3a34a605c
2langk <leelk980@gmail.com>
2023-10-09 19:19:02 +0900

    [feat] add @nestjs/swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule } from '@nestjs/swagger';
+import { join } from 'path';
 import { AppModule } from './app.module';
 import { environment } from './common/environment';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  SwaggerModule.setup('docs', app, require(join(__dirname, '../swagger.json')));
+
   await app.listen(environment.port);
 }
 

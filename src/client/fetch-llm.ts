0d51594c1c26472af7aaeb25abcaf81983f054e4
2langk <leelk980@gmail.com>
2023-12-05 17:46:20 +0900

    [feat] add url env

--- src/client/fetch-llm.ts
+++ src/client/fetch-llm.ts
@@ -1,5 +1,5 @@
 export const fetchLlm = async <T extends object>(uri: string, body: T) => {
-  const response = await fetch(`http://localhost:4000${uri}`, {
+  const response = await fetch(`${process.env.NEXT_PUBLIC_LLM_API_URL}${uri}`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
